a4fbfadc9fac8e30e552a4b2c17c5682d7290797
YEsYPenKoO <55316298+YEsYPenKoO@users.noreply.github.com>
2020-03-26 20:31:12 +0200

    changed1

--- Module2/task1.js
+++ Module2/task1.js
@@ -9,6 +9,7 @@
 // console.log(fruits[1]);
 
 // fruits[1] = 'kiwi';
+
 //console.log(fruits);
 
 // fruits[3] =  'pears';
@@ -22,7 +23,7 @@
 // let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
 // const score1 = 12;
 
-// for (let i = 0; i < test1.length; i++){
+// for (let i = 0; i < test1.length; i += 1){
 //     if(test1[i] <= score1){
 //         continue;
 //     }
@@ -34,7 +35,7 @@
 // let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
 // const score2 = 15;
 
-// for (let k = 0; k < test2.length; k++){
+// for (let k = 0; k < test2.length; i += 1){
 
 //     if(test2[k] <= score2){
 //         continue;
@@ -48,7 +49,7 @@
 // let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
 // const score3 = 16;
 
-// for (let j = 0; j < test3.length; j++){
+// for (let j = 0; j < test3.length; i += 1){
 //     if(test3[j] > score3){
 //         continue;
 //     }
@@ -59,7 +60,9 @@
 
 // let tests = [
 //     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+
 //     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+
 //     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
 // ];
 
