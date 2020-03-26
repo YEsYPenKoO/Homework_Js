f06a108666a6e5e132a142d791dcabac3c00e42b
YEsYPenKoO <55316298+YEsYPenKoO@users.noreply.github.com>
2020-03-26 19:45:10 +0200

    homework2

---
+++ Module2/task1.js
@@ -0,0 +1,69 @@
+'use strict'
+
+// ----------1
+
+// let fruits = ['apples','bananas','oranges'];
+
+// console.log(fruits);
+
+// console.log(fruits[1]);
+
+// fruits[1] = 'kiwi';
+//console.log(fruits);
+
+// fruits[3] =  'pears';
+// fruits[4] =  'grape';
+// fruits[5] =  'peaches';
+
+// console.log(fruits);
+
+// ----------2
+
+// let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
+// const score1 = 12;
+
+// for (let i = 0; i < test1.length; i++){
+//     if(test1[i] <= score1){
+//         continue;
+//     }
+//     console.log(test1[i]);
+// }
+
+// ----------3
+
+// let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+// const score2 = 15;
+
+// for (let k = 0; k < test2.length; k++){
+
+//     if(test2[k] <= score2){
+//         continue;
+//     }
+
+//     console.log(test2[k]);
+// }
+
+// ----------4
+
+// let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+// const score3 = 16;
+
+// for (let j = 0; j < test3.length; j++){
+//     if(test3[j] > score3){
+//         continue;
+//     }
+//     console.log(test3[j]);
+// }
+
+// ----------5
+
+// let tests = [
+//     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+//     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+//     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
+// ];
+
+
+// for (let test of tests) {
+//   console.log( test );
+// }
