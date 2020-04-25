8c03bdca0f1f730678750238043b2ae9d6963970
YEsYPenKoO <55316298+YEsYPenKoO@users.noreply.github.com>
2020-04-25 13:24:12 +0300

    homework_m_3

---
+++ Module3/index.js
@@ -0,0 +1,72 @@
+'use strict'
+
+// const object = {
+//     key: value,
+//     key: value // свойство состоит из ключа и значения
+// }
+
+// const frontend = {
+//     language_1: "HTML",
+//     language_2: "CSS",
+//     language_3: "Js"
+// }
+// console.log('frontend', frontend);
+
+// const student = {
+//     name: "Eva",
+//     age: 40,
+//     sex: 'F',
+//     course: 'frontend',
+//     passexam: true
+// }
+// console.log('student', student); 
+
+// // ОБРАЩЕНИЕ К ОБЪЕКТАМ
+
+// // ------1
+// console.log(frontend.language_1);
+
+// // ------2
+// console.log(frontend["language_1"]);
+
+
+// // ИЗМЕНЕНИЕ ОБЪЕКТА
+
+// student.name = 'Anya';
+// console.log(student.name)
+
+// // ДОБАВЛЕНИЕ
+
+// student.adress = 'Frunze, 7';
+// console.log(student.adress)
+
+// console.log('student', student);
+
+// // УДАЛЕНИЕ СВОЙСТВА
+
+// delete frontend['language_1'];
+// console.log('student', frontend);
+
+
+// const key = 'test';
+
+// const getKey = function() {
+//     return 'score';
+// }
+
+// const testResult = {
+//     key, // тут передаётся пара - ключ: значение
+//     [key]: '2', // тут запишется значение переменной
+//     [getKey()]: 22
+// }
+// console.log(testResult); //{test: "2", score: 22}
+
+
+const marvelHeroes = [
+    "Captain Marvel",
+    "Thanos",
+    "Black Panter",
+    "Goose the Cat",
+    "Black Widow",
+    "Captain America"
+  ];
