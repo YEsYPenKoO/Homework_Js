f9a234003d396a5400902ce7b78c0589a8ef48b0
YEsYPenKoO <55316298+YEsYPenKoO@users.noreply.github.com>
2020-05-11 23:00:57 +0300

    task1

--- Module5/task1.js
+++ Module5/task1.js
@@ -4,16 +4,13 @@
 //   this.login = login;
 //   this.email = email;
 // }
-
-
 // Account.prototype.getInfo = function(){
-//     return`Login: ${this.email}, Email: ${this.login}`;
+//     returnLogin: ${this.email}, Email: ${this.login}`;
 // }
 
-// console.log(Account.prototype.getInfo); //function
 
+// console.log(Account.prototype.getInfo); //function
 // const mango = new Account('Mangozedog', 'mango@dog.woof');
 // mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-
 // const poly = new Account('Poly','poly@mail.com');
-// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+// poly.getInfo(); //Login: Poly, Email: poly@mail.com
