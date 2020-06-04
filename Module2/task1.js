e53a104ceed263109b81557708c240bc427dde40
YEsYPenKoO <55316298+YEsYPenKoO@users.noreply.github.com>
2020-06-05 00:51:08 +0300

    changed

--- Module2/task1.js
+++ Module2/task1.js
@@ -1,72 +1,58 @@
-'use strict'
-
-// ----------1
-
-// let fruits = ['apples','bananas','oranges'];
-
-// console.log(fruits);
-
-// console.log(fruits[1]);
-
-// fruits[1] = 'kiwi';
-
-//console.log(fruits);
-
-// fruits[3] =  'pears';
-// fruits[4] =  'grape';
-// fruits[5] =  'peaches';
-
-// console.log(fruits);
-
-// ----------2
-
-// let test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
-// const score1 = 12;
-
-// for (let i = 0; i < test1.length; i += 1){
-//     if(test1[i] <= score1){
-//         continue;
-//     }
-//     console.log(test1[i]);
-// }
-
-// ----------3
-
-// let test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
-// const score2 = 15;
-
-// for (let k = 0; k < test2.length; i += 1){
-
-//     if(test2[k] <= score2){
-//         continue;
-//     }
-
-//     console.log(test2[k]);
-// }
-
-// ----------4
-
-// let test3 =[20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
-// const score3 = 16;
-
-// for (let j = 0; j < test3.length; i += 1){
-//     if(test3[j] > score3){
-//         continue;
-//     }
-//     console.log(test3[j]);
-// }
-
-// ----------5
-
-// let tests = [
-//     [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
-
-//     [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
-
-//     [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22]
-// ];
+"usee strict";
 
 
-// for (let test of tests) {
-//   console.log( test );
-// }
+console.log("TUSK_1");
+let fruits = ["apples", "bananas", "oranges"];
+console.log(fruits);
+console.log(fruits[1]);
+fruits[1] = "kiwi";
+console.log(fruits);
+fruits.push("pears", "grapes", "peaches");
+console.log(fruits);
+
+
+console.log("TUSK_2");
+const test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
+const score1 = 12;
+for (let i = 0; i < test1.length; i += 1) {
+  if (test1[i] < score1) {
+    continue;
+  }
+  console.log("pass mark", test1[i]);
+}
+
+
+console.log("TUSK_3");
+const test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
+const score2 = 15;
+for (let i = 0; i < test2.length; i += 1) {
+  if (test2[i] < score2) {
+    continue;
+  }
+  console.log("pass mark", test2[i]);
+}
+
+
+console.log("TUSK_4");
+const test3 = [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
+const score3 = 16;
+for (let i = 0; i < test3.length; i += 1) {
+  if (test3[i] < score3) {
+    continue;
+  }
+  console.log("pass mark", test3[i]);
+}
+
+
+console.log("TUSK_5");
+let tests = [
+  [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12],
+  [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20],
+  [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22],
+];
+for (let i = 0; i < tests.length; i += 1) {
+  console.log(tests[i]);
+  for (let j = 0; j < tests.length; j += 1) {
+    console.log(tests[i][j]);
+  }
+}
